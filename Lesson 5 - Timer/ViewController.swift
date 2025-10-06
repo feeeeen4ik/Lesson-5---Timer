2e38d1c8a6a9be0dbd7d5bab74f21082e5565f5e
feeeeen4ik <feeeeen4ik1994@gmail.com>
2025-10-06 22:08:33 +0500

    Initial Commit

---
+++ Lesson 5 - Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 5 - Timer
+//
+//  Created by Феликс on 06.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
